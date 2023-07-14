ce694e25c1d949939bf439885de73099bb04078f
Aleksandr Ruchkan <aruchkan@yandex.ru>
2023-07-14 11:49:34 +0300

    HW3 Done, corrected again

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         ArrayList<Integer> integers = new ArrayList<>();
         for (int i = 0; i<10; i++){
-            integers.add(new Random().nextInt(50));
+            integers.add(new Random().nextInt(10));
         }
         System.out.println("Список чисел: " + integers);
         int min = 51;
@@ -29,15 +29,31 @@ public class Main {
         }
         srarifm = srarifm/count;
 
-        for (int i = 0; i < integers.size(); i++){
-            if (integers.get(i) % 2 == 0){
-                integers.remove(i);
-            }
+        integers.removeIf(integer -> integer % 2 == 0);
 
-        }
         System.out.println("Список после удаления чисел, кратных двум: " + integers);
         System.out.println("Минимальное значение (до удаления четных) = " + min);
         System.out.println("Максимальное значение (до удаления четных) = " + max);
         System.out.println("Среднее (до удаления четных) = " + srarifm);
+
+        min = 51;
+        max = 0;
+        srarifm = 0;
+
+        for (int i = 0; i < integers.size(); i++) {
+            if (integers.get(i) < min) {
+                min = integers.get(i);
+            }
+            if (integers.get(i) > max) {
+                max = integers.get(i);
+            }
+            srarifm += integers.get(i);
+            count++;
+        }
+        srarifm = srarifm/count;
+
+        System.out.println("Минимальное значение (после удаления четных) = " + min);
+        System.out.println("Максимальное значение (после удаления четных) = " + max);
+        System.out.println("Среднее (после удаления четных) = " + srarifm);
     }
 }
